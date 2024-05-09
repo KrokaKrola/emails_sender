ff60fe046c486fd67371c213e1cf8b7474ffa56e
KrokaKrola <harlamsan@gmail.com>
2024-05-10 00:20:37 +0300

    feat: update path to routes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,11 @@
 package main
 
-import "github.com/krokakrola/emails_sender/internal/api"
+import (
+	"github.com/krokakrola/emails_sender/internal/api/router"
+)
 
 func main() {
-	router := api.NewApiRouter()
+	router := router.NewApiRouter()
 
 	router.InitRoutes()
 }
